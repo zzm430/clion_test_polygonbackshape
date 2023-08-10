92c856cb79f5f4d434791c909540fbb97f396d5d
zzmzzm <17791476352@163.com>
2023-08-10 09:24:57 +0800

    style:屏蔽无用代码

--- tools/testnode.py
+++ tools/testnode.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 #功能导入txt文件中的坐标点并显示其数据
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 fig, ax = plt.subplots()
-BB = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/origin_polygon.txt')
-BB_x = BB[0]
-BB_y = BB[1]
-ff = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/narrow_points.txt')
-ff_x = ff[0]
-ff_y = ff[1]
+# BB = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/origin_polygon.txt')
+# BB_x = BB[0]
+# BB_y = BB[1]
+# ff = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/narrow_points.txt')
+# ff_x = ff[0]
+# ff_y = ff[1]
 # AA = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/inter_nodes.txt')
 # AA_x = AA[0]
 # AA_y = AA[1]
@@ -18,15 +18,15 @@ ff_y = ff[1]
 CC = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/keypoints.txt')
 CC_x = CC[0]
 CC_y = CC[1]
-DD = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/realline.txt')
-DD_x = DD[0]
-DD_y = DD[1]
-EE = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/increaseNodes.txt')
-EE_x = EE[0]
-EE_y = EE[1]
-NN = np.loadtxt('/home/zzm/Desktop/reeds_shepp-master/RS_Lib/show_ridge_path11.txt')
-NN_x = NN[0]
-NN_y = NN[1]
+# DD = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/realline.txt')
+# DD_x = DD[0]
+# DD_y = DD[1]
+# EE = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/increaseNodes.txt')
+# EE_x = EE[0]
+# EE_y = EE[1]
+# NN = np.loadtxt('/home/zzm/Desktop/reeds_shepp-master/RS_Lib/show_ridge_path11.txt')
+# NN_x = NN[0]
+# NN_y = NN[1]
 # test = np.loadtxt('/home/zzm/Desktop/test_path_figure-main/src/test0620.txt')
 # test_x = test[0]
 # test_y = test[1]
@@ -36,7 +36,7 @@ NN_y = NN[1]
 # ax.plot(EE_x,EE_y,color='b',markerfacecolor='green',marker='o',label='increaseNodes data')
 # ax.plot(DD_x,DD_y,color='y',markerfacecolor='green',marker='o',label='inter_nodes data')
 # ax.plot(BB_x,BB_y,color='r',markerfacecolor='green',marker='o',label='origin_polygon data')
-# ax.plot(CC_x,CC_y,color='g',markerfacecolor='green',marker='o',label='keypoints data')
+ax.plot(CC_x,CC_y,color='g',markerfacecolor='green',marker='o',label='keypoints data',linewidth= 0.3,markersize=1)
 # ax.plot(test_x,test_y,color='g',markerfacecolor='green',marker='o',label='keypoints data')
 # ax.plot(NN_x,NN_y,color='g',markerfacecolor='green',marker='o',label='keypoints data')
 #
